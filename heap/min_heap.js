30ddeff4aea2d4824d953dd3621bf8fd7a25518f
houxinjie <houxinjie@gmail.com>
2016-12-13 11:58:00 +0800

    二叉堆，最大堆最小堆

---
+++ heap/min_heap.js
@@ -0,0 +1,73 @@
+class MinHeap{
+    constructor(data){
+        this.data = data;
+    }
+    insert(value){
+        this.data.push(value);
+        this._filterup(this.data.length - 1);
+    }
+    _filterup(current_index){
+        if(!current_index) {
+            current_index = this.data.length - 1;
+        }
+        let parent_index = Math.floor((current_index - 1) / 2);
+        let current = this.data[current_index];
+
+        while(current_index > 0){
+            if(this.data[parent_index] <= current) {
+                break;
+            } else {
+                this.data[current_index] = this.data[parent_index];
+                current_index = parent_index;
+                parent_index = Math.floor((parent_index - 1) / 2);
+            }
+        }
+        this.data[current_index] = current;
+    }
+
+    _filterdown(current_index){
+        let child_index = current_index * 2 + 1;
+        let current = this.data[current_index];
+        while(child_index <= this.data.length - 1) {
+            if(child_index < this.data.length - 1 && this.data[child_index] > this.data[child_index+1]){
+                child_index++;
+            }
+            if(current <= this.data[child_index]){
+                break;
+            } else {
+                this.data[current_index] = this.data[child_index];
+                current_index = child_index;
+                child_index = child_index * 2 + 1;
+            }
+        }
+        this.data[current_index] = current;
+    }
+
+    remove(data){
+        if(!this.data.length) return -1;
+
+        let index = this.data.indexOf(data);
+
+        if(index === -1) return -1;
+
+        let last = this.data.pop();
+
+        if(index !== this.data.length) {
+            this.data[index] = last;
+            this._filterdown(index);
+        }
+        return 0;
+    }
+
+    print(){
+        console.log(this.data);
+    }
+}
+
+
+let heap = new MinHeap([10,20,30,40,50,60,70,80,90]);
+heap.print();
+heap.insert(25);
+heap.print();
+heap.remove(20);
+heap.print();
